0dbedaaaf7cef988617bcd42e8619196416b7424
Mykhailo Yeroshenko <yeroshenko.m@icloud.com>
2023-06-30 10:42:27 +0300

    Created blank project

---
+++ AudiobookPlayer/AudiobookPlayer/AudiobookPlayerApp.swift
@@ -0,0 +1,18 @@
+//
+//  AudiobookPlayerApp.swift
+//  AudiobookPlayer
+//
+//  Author:  Mykhailo Yeroshenko
+//  Created: 30.06.2023
+//
+
+import SwiftUI
+
+@main
+struct AudiobookPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
